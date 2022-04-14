4ee3bc4d16cf1e940a29ac66fcc8ec2217231f9a
Hassan Sulaiman <haslek2013@icloud.com>
2022-04-14 16:17:33 +0100

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by user on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
